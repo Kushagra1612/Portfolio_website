bbeada7d4ffdc3a5b5784a946d308c837f045dff
Kushagra <kushagraverma1234@gmail.com>
2026-04-05 02:21:35 +0530

    Portfolio website 11

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -37,14 +37,14 @@ const ComputersCanvas = () => {
     window.matchMedia("(max-width: 768px)").matches
   );
 
-  // ✅ show static image on mobile
+  
   if (isMobile) {
     return (
-      <div className="w-full h-full flex items-center justify-center">
+      <div className="w-full h-full absolute inset-0 flex items-center justify-center">
         <img
           src={computerImg}
           alt="computer"
-          className="w-[280px] h-auto object-contain"
+          className="w-full h-full object-contain"
         />
       </div>
     );
